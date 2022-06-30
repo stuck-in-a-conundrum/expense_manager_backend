192db9c30271aa3d139d15813fbd3b207c5fe6cb
stuck-in-a-conundrum <bansal.drishti02@gmail.com>
2022-06-30 13:44:08 +0530

    heroku support

--- expense_manager/settings.py
+++ expense_manager/settings.py
@@ -150,6 +150,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = 'staticfiles/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+django_heroku.settings(locals())
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
